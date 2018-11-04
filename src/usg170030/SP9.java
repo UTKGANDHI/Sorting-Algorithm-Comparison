6ff2add5d1b7169b2d67cb26a6618dc62369a224
Utkarsh <utkarshgandhi96@gmail.com>
2018-11-04 16:55:02 -0600

    documentation done

--- src/usg170030/SP9.java
+++ src/usg170030/SP9.java
@@ -18,7 +18,7 @@ public class SP9 {
     public static int thresholdMergeSort3 = 99; // threshold for mergesort take 3
 
     public static void main(String[] args) {
-        int n = 32000000;  int choice = 1 ;//+ random.nextInt(4);
+        int n = 3200000;  int choice = 2 ;//+ random.nextInt(4);
         if(args.length > 0) { n = Integer.parseInt(args[0]); }
         if(args.length > 1) { choice = Integer.parseInt(args[1]); }
         int[] arr = new int[n];
---
+++ src/usg170030/readmeSP9.txt
@@ -0,0 +1,19 @@
+README file for SP 9 
+
+Authors:-
+ 1.  Utkarsh Gandhi (usg170030)
+ 2.  Sunny Bangale (shb170230)
+ 
+Steps for running the SP9.java file:
+
+1. Compile and Run the SP9.java file using the below commands:
+		> javac SP9.java
+		> java SP9 
+			
+		
+2. Alternatively it can be run in Intellij/ Eclipse or any Java Compatible IDE.
+
+ 
+ 
+ 
+ 
